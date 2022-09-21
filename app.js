2cafdba8ecb3b004665224e8b655d69e4d06923b
unknown <al_jager1983@yahoo.com>
2022-09-21 17:57:41 +0800

    udpate: using passportjs

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.use(express.static("public"))
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(session({
+    secret: "Our little secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
 
 const userSchema = new mongoose.Schema ({
@@ -23,7 +32,15 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose)
+
 const User = new mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy()) //create a local login strategy
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.get("/", function(req, res){
     res.render("home")
@@ -37,42 +54,52 @@ app.get("/register", function(req, res){
     res.render("register")
 })
 
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated){
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if(err){
+            return next(err)
+        }
+    })
+    res.redirect("/")
+})
+
 app.post("/register", function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username, 
-            password: hash
-        })
     
-        newUser.save(function(err) {
-            if(err){
-                console.log(err)
-            } else {
-                res.render("secrets")
-            }
-        })
-    });
-    
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err)
+            res.redirect("/register")                                   //go back to register page, then try again
+        } else {
+            passport.authenticate("local")(req, res, function(){        //if login is okay, then we authenticate using the ff. lines of codes
+                res.redirect("/secrets")
+            })    
+        }
+    })
+  
 })
 
 app.post("/login", function(req, res){
-    const username = req.body.username
-    const password = req.body.password
-
-    User.findOne({email: username}, function(err, foundUser){
-        if(err) {
-            console.log(err)
-        } else {
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-                // result == true
-                if(result === true) {
-                    res.render("secrets")
-                }
-            });
-                        
-        }
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
     })
+   req.login(user, function(err){
+    if(err){
+        console.log(err)
+    } else {
+        passport.authenticate("local")(req, res, function(){        //if login is okay, then we authenticate using the ff. lines of codes
+            res.redirect("/secrets")
+        })    
+    }
+   })
 })
 
 
